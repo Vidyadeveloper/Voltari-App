310d8213f5665aec5c80a3cacdbd22e163e4d85c
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-09-29 15:29:47 +0200

    Update lang.js

--- client/src/locale/lang.js
+++ client/src/locale/lang.js
@@ -1,7 +1,7 @@
 const LANG_KEY = "blazeLang";
 let currentLang = localStorage.getItem(LANG_KEY) || "en"; // <-- Use stored language
 let translations = {};
-
+let loaded = false;
 // List all translation files you want to load (without .json extension)
 const TRANSLATION_FILES = [
   "data-model/telemetry",
@@ -13,26 +13,38 @@ function getTranslationFilePaths(lang) {
     (file) => `/client/src/locale/${lang}/${file}.json`);
 }
 
-function loadTranslations(lang) {
+async function loadTranslations(lang) {
   const files = getTranslationFilePaths(lang);
-  console.log(files);
-  Promise.all(files.map((file) => fetch(file).then((res) => res.json())))
-    .then((results) => {
-      // Merge all loaded sections into one translations object
-      translations = results.reduce((acc, obj) => {
-        Object.assign(acc, obj);
-        return acc;
-      }, {});
-      document.dispatchEvent(new Event("lang-changed"));
-    });
+  const results = await Promise.all(files.map((file) => fetch(file).then(res => res.json())));
+  translations = results.reduce((acc, obj) => Object.assign(acc, obj), {});
+  loaded = true;
+  document.dispatchEvent(new Event("lang-changed"));
 }
-
+// function loadTranslations(lang) {
+//   const files = getTranslationFilePaths(lang);
+//   console.log(files);
+//   Promise.all(files.map((file) => fetch(file).then((res) => res.json())))
+//     .then((results) => {
+//       // Merge all loaded sections into one translations object
+//       translations = results.reduce((acc, obj) => {
+//         Object.assign(acc, obj);
+//         return acc;
+//       }, {});
+//       document.dispatchEvent(new Event("lang-changed"));
+//     });
+// }
 
 export function setLang(lang) {
   if (lang === currentLang) return;
   currentLang = lang;
+  localStorage.setItem(LANG_KEY, lang);
   loadTranslations(lang);
 }
+// export function setLang(lang) {
+//   if (lang === currentLang) return;
+//   currentLang = lang;
+//   loadTranslations(lang);
+// }
 
 export function getCurrentLang() {
   return currentLang;
@@ -46,6 +58,7 @@ export function t(section, key, fallback = "") {
  loadTranslations(currentLang);
 
 export { translations };
+
 
 
 
