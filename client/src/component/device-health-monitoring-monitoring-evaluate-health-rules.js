77e79a21c6e218b75e772f8946e7e10e9952036d
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-09-29 14:58:08 +0200

    Update device-health-monitoring-monitoring-evaluate-health-rules.js

--- client/src/component/device-health-monitoring-monitoring-evaluate-health-rules.js
+++ client/src/component/device-health-monitoring-monitoring-evaluate-health-rules.js
@@ -1,6 +1,6 @@
 import { FormDesignerComponent } from "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/form-designer-component.js";
 import "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/blaze-field-group.js";
-import { t } from "./../locale/lang.js";
+import { t,setLang } from "./../locale/lang.js";
 
 class DeviceHealthMonitoringMonitoringEvaluateHealthRules extends FormDesignerComponent {
   constructor() {
